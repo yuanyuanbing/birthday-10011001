985dce943b46f22a581b0a6f23cfcc8fe6b19666
yuanyuanbing <93985233+yuanyuanbing@users.noreply.github.com>
2022-10-01 12:21:48 +0100

    Add files via upload

--- pages/Part_2_🎷_The_cool_guy_Mischa.py
+++ pages/Part_2_🎷_The_cool_guy_Mischa.py
@@ -22,7 +22,9 @@ font-size:30px ; font-family: 'Cooper Black'; color: #FF9633;}
 st.markdown('<p class = "font1"> Welcome to Part_2.</p> \
 			<p>✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨</p>\
 			<p class = "font1">This part includes 2 MCQs and 3 Multi - select Questions.</p>\
-			<p class = "font1">Rules in Part_1 also applies here !! 🙂</p>', unsafe_allow_html = True)
+			<p class = "font1">Rules in Part_1 also applies here !! 🙂</p>\
+			<p>✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨</p>\
+			<p class = "font1">You will pass when balloon rise up again 🎈</p>', unsafe_allow_html = True)
 
 with st.form(key = 'form1'):
 	st.markdown('<p class = "font"> 1. Guess which month of summer (northern hemisphere seasons) is Bingbing\'s birthday in. </p>', unsafe_allow_html = True)
@@ -123,4 +125,4 @@ if anything:
 
 if anything and ((poem or travel or pass_out) and (not married)) and ((angel and toxic and candy) and (not complicated)) and day =='National day' and bir == 'June':
 	st.balloons()
-	st.markdown('<p class = "font2">Congratulations! You passed ! Contact Bingbing to get your present !</p>', unsafe_allow_html = True)
+	st.markdown('<p class = "font2">Congratulations ! You passed ! Contact Bingbing to get your present !</p>', unsafe_allow_html = True)
